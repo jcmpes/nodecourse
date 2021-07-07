17d79bc014b7223d7af078e52ab0f7356899ccf9
JC <jcmpes@gmail.com>
2021-07-07 11:37:18 +0200

    fix: added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 const loginController = require('./controllers/loginController');
 const registerController = require('./controllers/registerController');
 const forgotPasswordController = require('./controllers/forgotPasswordController');
@@ -17,6 +18,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
