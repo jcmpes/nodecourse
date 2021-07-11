d69e91eb8d6aa5e0589d10c805e034b4e780e7d6
Andrés <usuario616@gmail.com>
2021-07-11 12:43:06 +0200

    Feature: script to purge users with expired activation tokens

---
+++ purge_non_activated_accounts.js
@@ -0,0 +1,23 @@
+'use strict';
+const { mongoose, User } = require('./models');
+
+main().catch((err) => console.error(err));
+
+async function main() {
+  await purgeNonActivated();
+  mongoose.connection.close();
+}
+
+async function purgeNonActivated() {
+  const { deletedCount } = await User.deleteMany({
+    activated: false,
+    verifyTokenExpires: {
+      $lte: Date.now() - 1000 * 60 * 60,
+    },
+  });
+  console.log(
+    `Eliminado${deletedCount !== 1 ? 's' : ''} ${deletedCount} usuario${
+      deletedCount !== 1 ? 's' : ''
+    } inactivos.`,
+  );
+}
