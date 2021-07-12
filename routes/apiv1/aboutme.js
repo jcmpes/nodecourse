d55c0986a732e5677f23b2965bb97471d382c4be
Andrés <usuario616@gmail.com>
2021-07-12 19:05:18 +0200

    Fix: check favorite (one more time)

--- routes/apiv1/aboutme.js
+++ routes/apiv1/aboutme.js
@@ -10,6 +10,23 @@ const User = mongoose.model('User');
 const Favorite = mongoose.model('Favorite');
 
 /**
+ * Receives a course id and returns wether
+ * it's favorited by the user or not
+ *
+ * /api/v1/aboutme/isfav?course=60eaf69703f0a106fc605627
+ */
+router.get('/isfav/:course', jwtAuth, async function (req, res, next) {
+  try {
+    const user = req.apiAuthUserId;
+    const course = req.params.course;
+    const isFav = await Favorite.find({ user, course });
+    res.json({ result: isFav.length > 0 });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/**
 
  */
 router.get('/', jwtAuth, async function (req, res, next) {
@@ -17,26 +34,6 @@ router.get('/', jwtAuth, async function (req, res, next) {
     const _id = req.apiAuthUserId;
     const result = await User.findOne({ _id });
     res.json({ username: result.username });
-  } catch (err) {
-    next(err);
-  }
-});
-
-/**
- * Receives a course id and returns wether
- * it's favorited by the user or not
- *
- * /api/v1/aboutme/isfav?course=60eaf69703f0a106fc605627
- */
-router.get('/isfav', jwtAuth, async function (req, res, next) {
-  try {
-    const user = req.apiAuthUserId;
-    const { course } = req.body;
-    const isFav = await Favorite.findOne({ user, course });
-    if (!isFav) {
-      return res.json({ result: false });
-    }
-    res.json({ result: true });
   } catch (err) {
     next(err);
   }
