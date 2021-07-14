d88c5e7e77f28cf39ff5ebe2412a9a60226fb0c3
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-07-14 18:56:54 +0200

    Feat: Filter courses by title

--- models/Course.js
+++ models/Course.js
@@ -6,25 +6,28 @@ const slug = require('mongoose-slug-generator');
 // Initialize
 mongoose.plugin(slug);
 
-const courseSchema = mongoose.Schema({
-  title: { type: String, unique: true },
-  slug: { type: String, slug: "title", unique: true },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
+const courseSchema = mongoose.Schema(
+  {
+    title: { type: String, unique: true },
+    slug: { type: String, slug: 'title', unique: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Category',
+    },
+    featuredImage: String,
+    video: String,
+    description: String,
+    content: String,
   },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Category',
-  },
-  featuredImage: String,
-  video: String,
-  description: String,
-  content: String,
-}, { timestamps: true });
+  { timestamps: true },
+);
 
-courseSchema.statics.list = async function (skip, limit, sort) {
-  const query = Course.find().populate('user').populate('category');
+courseSchema.statics.list = async function (filter, skip, limit, sort) {
+  const query = Course.find(filter).populate('user').populate('category');
   query.sort(sort);
   query.skip(skip);
   query.limit(limit);
