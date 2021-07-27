9182ca55fa9550f353b181a7c137f0b366cc8b93
oscarCorb <info@oscarcorb.com>
2021-07-27 15:28:31 +0200

    fix: purchases endpoints

--- routes/apiv1/purchases.js
+++ routes/apiv1/purchases.js
@@ -9,38 +9,40 @@ const Course = mongoose.model('Course');
 const jwtAuth = require('../../lib/jwAuth');
 
 /**
- * Get /api/v1/purchases/id
- * return one purchase by id
+ * Get /api/v1/purchases/user
+ * Return all purchases for a specific user
  */
-router.get('/:id', jwtAuth, async function (req, res, next) {
+router.get('/user', jwtAuth, async function (req, res, next) {
   try {
-    const _id = req.params.id;
-    const purchase = await Purchase.findOne({ _id });
-    res.json({ purchase });
+    const userId = req.apiAuthUserId;
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      res.status(404).json({ error: 'user not found' });
+      return;
+    }
+    const purchases = await Purchase.find({ username: userId });
+    if (purchases.length < 1) {
+      res.json({ error: 'no purchases found for this user' });
+      return;
+    }
+    res.json({ userPurchases: purchases });
   } catch (error) {
     next(error);
   }
 });
 
 /**
- * Get /api/v1/purchases/user/username
- * Return all purchases for a specific user
+ * Get /api/v1/purchases/id
+ * return one purchase by id
  */
-router.get('/user/:username', jwtAuth, async function (req, res, next) {
+router.get('/:purchaseId', jwtAuth, async function (req, res, next) {
   try {
-    const { username } = req.params;
-    const user = await User.findOne({ username });
-    if (!user) {
-      res.status(404).json({ error: 'user not found' });
-      return;
-    }
-    const { _id } = user;
-    const purchases = await Purchase.find({ user: _id });
-    if (purchases.length < 1) {
-      res.json({ error: 'no purchases found for this user' });
-      return;
-    }
-    res.json({ userPurchases: purchases });
+    const purchaseId = req.params.purchaseId;
+    const userId = req.apiAuthUserId;
+    // TODO comprobar que el usuario es correcto
+    const purchase = await Purchase.findOne({ _id: purchaseId });
+    console.log('purchase', purchase);
+    res.json({ purchase });
   } catch (error) {
     next(error);
   }
@@ -119,10 +121,32 @@ router.post('/', jwtAuth, async function (req, res, next) {
  */
 router.delete('/:purchaseId', jwtAuth, async function (req, res, next) {
   try {
+    const userId = req.apiAuthUserId;
     const { purchaseId } = req.params;
+
+    // delete purchase from DB
     const deletedPurchase = await Purchase.findOneAndRemove({
       _id: purchaseId,
     });
+    if (!deletedPurchase) {
+      res.status(404).json({ error: 'not found' });
+      return;
+    }
+
+    // delete user purchased courses that match with deleted purchase
+    const coursesToDelete = deletedPurchase.purchasedCourses;
+    // get all user purchased courses and empty the array
+    const userCourses = await User.findOneAndUpdate(
+      { _id: userId },
+      { courses: [] },
+    );
+    // filter valid courses
+    const userValidCourses = userCourses.courses.filter(
+      (course) => !coursesToDelete.includes(course),
+    );
+    // put valid courses in User array
+    await User.findOneAndUpdate({ _id: userId }, { courses: userValidCourses });
+
     res.status(200).json({ deletedPurchase });
   } catch (error) {
     next(error);
