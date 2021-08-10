e6b29d82323050e32dbd459c41abbbbeca88615a
Andrés <usuario616@gmail.com>
2021-08-10 15:39:30 +0200

    feat: edit user details

--- routes/apiv1/users.js
+++ routes/apiv1/users.js
@@ -24,4 +24,24 @@ router.get('/:username', async function (req, res, next) {
   }
 });
 
+router.put('/edit', jwtAuth, async function (req, res, next) {
+  try {
+    const _id = req.apiAuthUserId;
+    const formData = { ...req.body };
+    console.log(formData);
+    const user = await User.findOne({ _id });
+    if (!user) {
+      return res.status(404).json({ error: 'not found' });
+    }
+    if (formData.username) user.username = formData.username;
+    if (formData.email) user.email = formData.email;
+    if (formData.password)
+      user.password = await User.hashPassword(formData.password);
+    const result = await user.save();
+    res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
