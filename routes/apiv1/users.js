77e12f10fda5549dbf1b988cbeab0a18e3b842b8
Andrés <usuario616@gmail.com>
2021-08-11 15:35:15 +0200

    feat: delete account

--- routes/apiv1/users.js
+++ routes/apiv1/users.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Course = mongoose.model('Course');
 const User = mongoose.model('User');
+const Favorite = mongoose.model('Favorite');
 const jwtAuth = require('../../lib/jwAuth');
 
 router.get('/:username', async function (req, res, next) {
@@ -28,7 +29,6 @@ router.put('/edit', jwtAuth, async function (req, res, next) {
   try {
     const _id = req.apiAuthUserId;
     const formData = { ...req.body };
-    console.log(formData);
     const user = await User.findOne({ _id });
     if (!user) {
       return res.status(404).json({ error: 'not found' });
@@ -39,6 +39,47 @@ router.put('/edit', jwtAuth, async function (req, res, next) {
       user.password = await User.hashPassword(formData.password);
     const result = await user.save();
     res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/delete-account', jwtAuth, async function (req, res, next) {
+  try {
+    const _id = req.apiAuthUserId;
+    const { password } = req.body;
+
+    const user = await User.findOne({ _id });
+    if (!user) {
+      return res.status(404).json({ error: 'not found' });
+    }
+
+    if (await user.comparePassword(password)) {
+      await User.findOneAndDelete({ _id }, function (err, result) {
+        if (err) {
+          res.status(500).json({ delete: 'fail' });
+        } else {
+          Course.deleteMany({ user: _id }).then(({ deletedCount }) => {
+            console.log(
+              `Deleted ${deletedCount} course${deletedCount !== 1 ? 's' : ''}`,
+            );
+          });
+          Favorite.deleteMany({ fav: { user: _id } }).then(
+            ({ deletedCount }) => {
+              console.log(
+                `Deleted ${deletedCount} favorite${
+                  deletedCount !== 1 ? 's' : ''
+                }`,
+              );
+            },
+          );
+
+          res.status(200).json({ deleted: result });
+        }
+      });
+    } else {
+      res.status(401).json({ delete: 'unauthorized' });
+    }
   } catch (err) {
     next(err);
   }
