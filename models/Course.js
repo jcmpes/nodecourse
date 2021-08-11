7d0299fd8ccfb3b637937c0a89a9b905c43aa682
Manuel Hernandez <cmanuelhm87@gmail.com>
2021-08-11 20:25:46 +0200

    Fix For filters

--- models/Course.js
+++ models/Course.js
@@ -35,8 +35,7 @@ const courseSchema = mongoose.Schema(
 
 courseSchema.statics.list = async function (filter, skip, limit, sort) {
   const query = Course.find(filter).populate('user').populate('category');
-  console.log('Sort ', sort);
-  query.sort({ _id: sort });
+  query.sort(sort);
   query.skip(skip);
   query.limit(limit);
 
